7b56fb1c13570a12b9dcc7f1b47ccba162d9374c
Mehul Mohan <mehulmpt@gmail.com>
2021-03-25 04:52:28 +0530

    work

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function calculator(num1, num2, operation) {
-	// return a Promise
+	// return a Promsie
+	return new Promise((resolve, reject) => {
+		if (operation === '+') {
+			resolve(num1 + num2)
+		} else if (operation === '-') {
+			resolve(num1 - num2)
+		} else {
+			reject('error')
+		}
+	})
 }
 
 module.exports = calculator
