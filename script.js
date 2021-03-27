7242c38f83d2913a447f8514a92c7a18654f2f0f
Mehul Mohan <mehulmpt@gmail.com>
2021-03-27 22:48:09 +0530

    work

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // write your code here
+const button1 = document.getElementById('my-button')
+const button2 = document.getElementById('my-second-button')
+const output = document.getElementById('output')
+
+button1.onclick = () => {
+	output.innerText = button1.innerText
+}
+
+button2.onclick = () => {
+	output.innerText = button2.innerHTML
+}
