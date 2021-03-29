998d1ab2ffb18611b06a9efc2ed084c0ac05c38a
Mehul Mohan <mehulmpt@gmail.com>
2021-03-29 19:13:00 +0530

    work

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // write your code here
+const programmingLanguages = []
+
+programmingLanguages.push('html')
+programmingLanguages.push('css')
+programmingLanguages.push('javascript')
+
+programmingLanguages2 = [...programmingLanguages]
+
+programmingLanguages2.pop()
+programmingLanguages2.shift()
+
+const index1 = programmingLanguages.indexOf('css')
+const index2 = programmingLanguages2.indexOf('css')
+
+console.log(programmingLanguages2)
