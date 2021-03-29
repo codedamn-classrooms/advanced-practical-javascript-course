16a0f82128081e503cf89716a785b58fffe2956f
Mehul Mohan <mehulmpt@gmail.com>
2021-03-29 19:06:05 +0530

    work

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-const btn1 = document.getElementById('my-button')
-const btn2 = document.getElementById('my-second-button')
-const output = document.getElementById('output')
-const input = document.getElementById('my-input')
-
-btn1.addEventListener('click', swapInput)
-btn2.addEventListener('click', clearListener)
-
-function swapInput() {
-	output.innerText = input.value
-}
-
-function clearListener() {
-	btn1.removeEventListener('click', swapInput)
-}
+// write your code here
