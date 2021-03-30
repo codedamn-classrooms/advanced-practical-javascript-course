9dfb7c195c8f6d794b05f9c1970f507f18f59250
Mehul Mohan <mehulmpt@gmail.com>
2021-03-31 03:50:33 +0530

    work

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // write your code here
+function Car(fuel) {
+	this.fuel = fuel
+}
+
+Car.prototype.weather = 0
+Car.prototype.getDistanceCarCanTravel = function () {
+	if (this.weather === 0) {
+		return this.fuel * 10
+	} else if (this.weather === 1) {
+		return this.fuel * 5
+	} else {
+		return this.fuel * 2
+	}
+}
