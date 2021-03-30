1871f07a6cfd5ffc6303d720c9a2ef586fc5a98a
Mehul Mohan <mehulmpt@gmail.com>
2021-03-31 03:15:52 +0530

    work

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// write your code here
-const programmingLanguages = []
-
-programmingLanguages.push('html')
-programmingLanguages.push('css')
-programmingLanguages.push('javascript')
-
-programmingLanguages2 = [...programmingLanguages]
-
-programmingLanguages2.pop()
-programmingLanguages2.shift()
-
-const index1 = programmingLanguages.indexOf('css')
-const index2 = programmingLanguages2.indexOf('css')
-
-console.log(programmingLanguages2)
+function addOrDeleteProp(object, propertyName) {
+	// write your code here
+}
