629f8e9d192fb4c68a4ca1ec10f114ef1140a072
Mehul Mohan <mehulmpt@gmail.com>
2021-03-31 03:45:03 +0530

    work

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-function addOrDeleteProp(object, propertyName) {
-	// write your code here
-	if (object.hasOwnProperty(propertyName)) {
-		delete object[propertyName]
-		return object
-	} else {
-		object[propertyName] = 'objects are cool'
-	}
-
-	return object
-}
+// write your code here
