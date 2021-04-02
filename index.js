b5cb561a9a8a0f3bb51642f3d5e7bb4f949c6e6f
Mehul Mohan <mehulmpt@gmail.com>
2021-04-02 19:40:56 +0530

    work

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function fn(...promises) {
 	// resolve them all
-	// add one to all of them
+
+	return Promise.allSettled(promises).then((resolvedRejectedValues) => {
+		const values = []
+		resolvedRejectedValues.forEach((promise) => {
+			if (promise.status === 'fulfilled') {
+				values.push(promise.value * 2)
+			} else {
+				values.push(null)
+			}
+		})
+
+		return values
+	})
 }
 
 module.exports = fn
