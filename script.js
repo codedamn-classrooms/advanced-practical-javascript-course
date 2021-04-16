1e308fc92f5faec1c69cc7247c71eea141b55083
Mehul Mohan <mehulmpt@gmail.com>
2021-04-16 20:58:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 function addOrDeleteProp(object, propertyName) {
 	// write your code here
-	if (object.hasOwnProperty(propertyName)) {
-		delete object[propertyName]
-		return object
-	} else {
-		object[propertyName] = 'objects are cool'
-	}
-
-	return object
+	
 }
