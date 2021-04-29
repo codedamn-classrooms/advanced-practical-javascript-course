a9c55bbc4809a35da002f4243c2e22d4e5bc0c98
Mehul Mohan <mehulmpt@gmail.com>
2021-04-29 22:09:24 +0530

    solution

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fn(customPromise) {
 	// return a new Promsie which resolves to + 1 of the resolved promise value
+	return customPromise.then((value) => {
+		return value + 1
+	})
 }
 
 module.exports = fn
