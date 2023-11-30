db5dd2c642bea5774b41075120372cb319a7219e
Pranav <pranav.techiegeek@gmail.com>
2023-11-30 16:54:12 +0530

    ready

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-function fn(...promises) {
-	// resolve them all
-	// add one to all of them
+/**
+ * Returns an array of doubled value of the resolved values of the promises
+ * @param  {...any} promises
+ * @returns {Array}
+ */
+async function fn(...promises) {
+	// resolve all the promises and returned the doubled value of each Promise
 }
 
-module.exports = fn
+// Don't change this line - test cases need the function to be exported
+module.exports = fn;
+
+// Sample Tests
+fn(Promise.resolve(2), Promise.resolve(4)).then((data) => console.log(data)); // should return [4,6,8]
+fn(Promise.resolve(2)).then((data) => console.log(data)); // should return [4]
+// fn(Promise.reject(), Promise.resolve(5)).then((data) => console.log(data)); // should throw an error
